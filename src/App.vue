e151e57988fe79597a9c173bdb03155557478b7b
michele-pupo <mikypupo@hotmail.it>
2024-03-06 11:39:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import AppNavbar from './components/AppNavbar.vue';
+
+  export default{
+
+    components: {
+      AppNavbar
+    },
+  }
+</script>
+
+<template>
+  <AppNavbar></AppNavbar>
+</template>
+
+<style lang="scss">
+  @use './styles/general.scss' as *;
+</style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,13 @@
+<script>
+    export default{
+        name: 'Appnavbar',
+    }
+</script>
+
+<template>
+  <h1>ciao</h1>
+</template>
+
+<style lang="scss">
+  
+</style>
