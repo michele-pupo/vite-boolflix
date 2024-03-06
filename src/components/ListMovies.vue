7231db56ddf4f51764dd81a3de0f850418b19e92
michele-pupo <mikypupo@hotmail.it>
2024-03-06 18:11:43 +0100

    set flags for movies

--- src/components/ListMovies.vue
+++ src/components/ListMovies.vue
@@ -19,7 +19,10 @@
   <ul>
     <Movie 
         v-for="movie in store.movies"
-        :title="movie.title">  
+        :title="movie.title"
+        :originalTitle="movie.original_title"
+        :originalLanguage="movie.original_language"
+        :vote="Math.ceil(movie.vote_average)">  
     </Movie>
   </ul>
 
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -10,7 +10,30 @@
         },
         props: {
           title: String,
+          originalTitle: String,
+          originalLanguage: String,
+          vote: Number,
+        },
+        computed: {
+          languageFlagUrl() {
+            // Mappa tra le stringhe restituite dall'API e i percorsi delle immagini delle bandiere
+            const flagMapping = {
+              // Esempio di mappatura
+              'en': '../../public/img/en.jpg', // Percorso dell'immagine della bandiera inglese
+              'fr': '../../public/img/fr.jpg',
+              'de': '../../public/img/de.jpg',
+              'es': '../../public/img/es.jpg',
+              'it': '../../public/img/it.jpg',
+              'ja': '../../public/img/ja.jpg',
+              'ko': '../../public/img/ko.jpg',
+              'pl': '../../public/img/pl.jpg' // Percorso dell'immagine della bandiera francese
+              // Aggiungi altre corrispondenze necessarie
+            };
+
+            // Ottieni il percorso dell'immagine della bandiera corrispondente alla lingua originale
+            return flagMapping[this.originalLanguage] || '../../public/img/BrandAssets_Logos_01-Wordmark.jpg'; // Se la lingua non è mappata, usa una bandiera predefinita
         }
+      }
     }
 </script>
 
@@ -22,6 +45,10 @@
     </div>
     <div id="description-movie">
         <h3>{{ title }}</h3>
+        <h2>{{ originalTitle }}</h2>
+        <img :src="languageFlagUrl" alt="Language Flag">
+        <h4>{{ originalLanguage }}</h4>
+        <h5>{{ vote }}</h5>
     </div>
 
   </li>
